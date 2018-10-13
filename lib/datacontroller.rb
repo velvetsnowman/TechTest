052e8aded94876400060d2f692a59c20ddbde662
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-13 18:20:31 +0100

    implemented function for retrieving all data from table

---
+++ lib/datacontroller.rb
@@ -0,0 +1,35 @@
+require 'pg'
+
+class Datacontroller
+
+  def initialize(id, product, customer, measure, value, valid_from, valid_to)
+    @id = id
+    @product = product
+    @customer = customer
+    @measure = measure
+    @value = value
+    @valid_from = valid_from
+    @valid_to = valid_to
+  end
+
+  def self.get_all
+    data_sets = []
+    connection = PG.connect(dbname: 'product_data')
+    result = connection.exec("SELECT * FROM data")
+    result.each do |row|
+      data_sets << Datacontroller.new(row['id'], row['product'], row['customer'], row['measure'], row['value'], row['valid_from'], row['valid_to'])
+    end
+    return data_sets
+  end
+
+  def self.search
+    data_sets = []
+    connection = PG.connect(dbname: 'product_data')
+    result = connection.exec("SELECT * FROM data")
+    result.each do |row|
+      @data_sets << Datacontroller.new(row['id'], row['product'], row['customer'], row['measure'], row['value'], row['valid_from'], row['valid_to'])
+    end
+    return data_sets
+  end
+
+end
