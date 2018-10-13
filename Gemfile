967128c114c13babc71d33057dbb8f38ecda9719
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-13 18:44:18 +0100

    can now query db based on desired prod,cust,meaure

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '2.5.1'
+
+gem 'capybara'
+gem 'pg'
+gem 'rspec'
+gem 'rubocop'
+gem 'sinatra'
+gem 'sinatra-flash'
--- lib/datacontroller.rb
+++ lib/datacontroller.rb
@@ -22,12 +22,12 @@ class Datacontroller
     return data_sets
   end
 
-  def self.search
+  def self.search(product, customer, measure)
     data_sets = []
     connection = PG.connect(dbname: 'product_data')
-    result = connection.exec("SELECT * FROM data")
+    result = connection.exec("SELECT * FROM data WHERE product='#{product}' AND customer='#{customer}' AND measure='#{measure}' ORDER BY valid_to ASC")
     result.each do |row|
-      @data_sets << Datacontroller.new(row['id'], row['product'], row['customer'], row['measure'], row['value'], row['valid_from'], row['valid_to'])
+      data_sets << Datacontroller.new(row['id'], row['product'], row['customer'], row['measure'], row['value'], row['valid_from'], row['valid_to'])
     end
     return data_sets
   end
