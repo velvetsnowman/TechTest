39c8a4c9cfb02a1dd46a699ca547e18f4e2c186f
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-14 23:09:51 +0100

    can now update all data

--- app.rb
+++ app.rb
@@ -17,18 +17,23 @@ class Datamanager < Sinatra::Base
     erb(:search)
   end
 
-  get '/search_results' do
+  get '/results' do
     session[:product] = params['product']
     session[:customer] = params['customer']
     session[:measure] = params['measure']
     @data_sets = Datacontroller.search(params['product'], params['customer'], params['measure'])
-    erb(:search_results)
+    erb(:results)
   end
 
   get '/updated_results' do
-    Datacontroller.check_errors
+    Datacontroller.check_searched_errors
     @data_sets = Datacontroller.search(session[:product], session[:customer], session[:measure])
     erb(:updated_results)
+  end
+
+  get '/all_results' do
+    Datacontroller.check_all_errors
+    redirect '/all'
   end
 
   run! if app_file == $PROGRAM_NAME
--- lib/datacontroller.rb
+++ lib/datacontroller.rb
@@ -51,23 +51,47 @@ class Datacontroller
                                           row['valid_to'])}
   end
 
-  def self.check_errors
-    @data_set = []
+  def self.check_searched_errors
+    data_set = []
     connection = PG.connect(dbname: 'product_data')
     result = connection.exec("SELECT id, valid_from, valid_to FROM data WHERE
                               product='#{@product}'
                               AND customer='#{@customer}'
                               AND measure='#{@measure}'
                               ORDER BY valid_from ASC")
-    result.map { |row| @data_set << row }
+    result.map { |row| data_set << row }
     i = 1
-    while @data_set.length > i do
-      if (DateTime.parse(@data_set[i-1]["valid_from"])..DateTime.parse(@data_set[i-1]["valid_to"])).cover?(DateTime.parse(@data_set[i]["valid_from"]))
-        x = (DateTime.parse(@data_set[i]["valid_from"]) -1).strftime('%Y-%m-%d')
-        y = @data_set[i-1]["id"]
+    while data_set.length > i do
+      if (DateTime.parse(data_set[i-1]["valid_from"])..DateTime.parse(data_set[i-1]["valid_to"])).cover?(DateTime.parse(data_set[i]["valid_from"]))
+        new_valid_to = (DateTime.parse(data_set[i]["valid_from"]) -1).strftime('%Y-%m-%d')
+        row_id = data_set[i-1]["id"]
         connection.exec("UPDATE data
-                        SET valid_to = '#{x}'
-                        WHERE id= #{y}")
+                        SET valid_to = '#{new_valid_to}'
+                        WHERE id= #{row_id}")
+      end
+      i += 1
+    end
+  end
+
+  def self.check_all_errors
+    data_set = []
+    connection = PG.connect(dbname: 'product_data')
+    result = connection.exec("SELECT * FROM data
+                              ORDER BY customer ASC,
+                              product ASC,
+                              measure ASC,
+                              valid_from ASC")
+    result.map { |row| data_set << row }
+    i = 1
+    while data_set.length > i do
+      if data_set[i]["product"] == data_set[i-1]["product"] &&
+        data_set[i]["customer"] == data_set[i-1]["customer"] &&
+        data_set[i]["measure"] == data_set[i-1]["measure"] && (DateTime.parse(data_set[i-1]["valid_from"])..DateTime.parse(data_set[i-1]["valid_to"])).cover?(DateTime.parse(data_set[i]["valid_from"]))
+        new_valid_to = (DateTime.parse(data_set[i]["valid_from"]) -1).strftime('%Y-%m-%d')
+        row_id = data_set[i-1]["id"]
+        connection.exec("UPDATE data
+                        SET valid_to = '#{new_valid_to}'
+                        WHERE id= #{row_id}")
       end
       i += 1
     end
