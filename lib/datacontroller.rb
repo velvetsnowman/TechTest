3f2ddc6d8d3be862fbffb55c32a871468fa08512
Daniel den Hartog <daniel.mista@gmail.com>
2018-10-16 01:16:10 +0100

    updated sql queries to add timstamp and og values

--- lib/datacontroller.rb
+++ lib/datacontroller.rb
@@ -63,17 +63,22 @@ class Datacontroller
     i = 1
     while data_set.length > i do
       if (DateTime.parse(data_set[i-1]["valid_from"])..DateTime.parse(data_set[i-1]["valid_to"])).cover?(DateTime.parse(data_set[i]["valid_from"]))
-        new_valid_to = (DateTime.parse(data_set[i]["valid_from"]) -1).strftime('%Y-%m-%d')
+
+        new_valid_to = (DateTime.parse(data_set[i]["valid_from"]) -1).strftime('%Y%m%d')
+        original_valid_to = (DateTime.parse(data_set[i-1]["valid_to"])).strftime('%Y%m%d')
+        time_now = (Time.now).strftime("%Y%m%d")
         row_id = data_set[i-1]["id"]
         connection.exec("UPDATE data
-                        SET valid_to = '#{new_valid_to}'
+                        SET valid_to = '#{new_valid_to}',
+                        original_valid_to = '#{original_valid_to}',
+                        time_updated = '#{time_now}'
                         WHERE id= #{row_id}")
       end
       i += 1
     end
   end
 
-  def self.check_all_errors
+  def self.check_all_errors #here
     data_set = []
     connection = PG.connect(dbname: 'product_data')
     result = connection.exec("SELECT * FROM data
@@ -86,11 +91,17 @@ class Datacontroller
     while data_set.length > i do
       if data_set[i]["product"] == data_set[i-1]["product"] &&
         data_set[i]["customer"] == data_set[i-1]["customer"] &&
-        data_set[i]["measure"] == data_set[i-1]["measure"] && (DateTime.parse(data_set[i-1]["valid_from"])..DateTime.parse(data_set[i-1]["valid_to"])).cover?(DateTime.parse(data_set[i]["valid_from"]))
-        new_valid_to = (DateTime.parse(data_set[i]["valid_from"]) -1).strftime('%Y-%m-%d')
+        data_set[i]["measure"] == data_set[i-1]["measure"] &&
+        (DateTime.parse(data_set[i-1]["valid_from"])..DateTime.parse(data_set[i-1]["valid_to"])).cover?(DateTime.parse(data_set[i]["valid_from"]))
+
+        original_valid_to = (DateTime.parse(data_set[i-1]["valid_to"])).strftime('%Y%m%d')
+        new_valid_to = (DateTime.parse(data_set[i]["valid_from"]) -1).strftime('%Y%m%d')
+        time_now = (Time.now).strftime("%Y%m%d")
         row_id = data_set[i-1]["id"]
         connection.exec("UPDATE data
-                        SET valid_to = '#{new_valid_to}'
+                        SET valid_to = '#{new_valid_to}',
+                        original_valid_to = '#{original_valid_to}'
+                        time_updated = '#{time_now}'
                         WHERE id= #{row_id}")
       end
       i += 1
